bd3a441eb8629a132dac0494aab2c4f5831621f3
Sulaimon <abodunrinsulaimon1@gmail.com>
2025-09-13 10:12:07 +0100

    More Update: [A Readme, requirements and gitignore files was added] git ignore file to hide virtual environment

--- gui_calculator_2/gui_calculator_prototype.py
+++ gui_calculator_2/gui_calculator_prototype.py
@@ -32,65 +32,65 @@ class Calculator(QWidget):
         self.initui()
 
     def initui(self):
-#Calculator screen
-     vbox=QVBoxLayout()
+    #Calculator screen
+    vbox=QVBoxLayout()
 
-     vbox.addWidget(self.output)
+    vbox.addWidget(self.output)
 
- #row1
-     row1=QHBoxLayout()
-     row1.addWidget(self.digit)
-     row1.addWidget(self.digit__)
-     row1.addWidget(self.digit___)
-     row1.addWidget(self.digit____)
-     row1.addWidget(self.backspace)
-     row1.addWidget(self.clear)
-     vbox.addLayout(row1)
-#row2
-     row2=QHBoxLayout()
-     row2.addWidget(self.digit0)
-     row2.addWidget(self.digit1)
-     row2.addWidget(self.digit2)
-     row2.addWidget(self.digit3)
-     vbox.addLayout(row2)
-#row3
-     row3=QHBoxLayout()
-     row3.addWidget(self.digit4)
-     row3.addWidget(self.digit5)
-     row3.addWidget(self.digit6)
-     row3.addWidget(self.digit7)
-     vbox.addLayout(row3)
-#row4
-     row4=QHBoxLayout()
-     row4.addWidget(self.digit8)
-     row4.addWidget(self.digit9)
-     row4.addWidget(self.digit00)
-     row4.addWidget(self.digit_)
-     vbox.addLayout(row4)
+     #row1
+    row1=QHBoxLayout()
+    row1.addWidget(self.digit)
+    row1.addWidget(self.digit__)
+    row1.addWidget(self.digit___)
+    row1.addWidget(self.digit____)
+    row1.addWidget(self.backspace)
+    row1.addWidget(self.clear)
+    vbox.addLayout(row1)
+    #row2
+    row2=QHBoxLayout()
+    row2.addWidget(self.digit0)
+    row2.addWidget(self.digit1)
+    row2.addWidget(self.digit2)
+    row2.addWidget(self.digit3)
+    vbox.addLayout(row2)
+    #row3
+    row3=QHBoxLayout()
+    row3.addWidget(self.digit4)
+    row3.addWidget(self.digit5)
+    row3.addWidget(self.digit6)
+    row3.addWidget(self.digit7)
+    vbox.addLayout(row3)
+    #row4
+    row4=QHBoxLayout()
+    row4.addWidget(self.digit8)
+    row4.addWidget(self.digit9)
+    row4.addWidget(self.digit00)
+    row4.addWidget(self.digit_)
+    vbox.addLayout(row4)
 
-     self.setLayout(vbox)
-     self.output.setMinimumHeight(60)
+    self.setLayout(vbox)
+    self.output.setMinimumHeight(60)
 
-     self.digit.clicked.connect(lambda:self.output.insert('+'))
-     self.digit__.clicked.connect(lambda:self.output.insert('-'))
-     self.digit___.clicked.connect(lambda:self.output.insert('*'))
-     self.digit____.clicked.connect(lambda:self.output.insert('/'))
-     self.digit0.clicked.connect(lambda: self.output.insert('0'))
-     self.digit00.clicked.connect(lambda: self.output.insert('00'))
-     self.digit1.clicked.connect(lambda: self.output.insert('1'))
-     self.digit2.clicked.connect(lambda: self.output.insert('2'))
-     self.digit3.clicked.connect(lambda: self.output.insert('3'))
-     self.digit4.clicked.connect(lambda: self.output.insert('4'))
-     self.digit5.clicked.connect(lambda: self.output.insert('5'))
-     self.digit6.clicked.connect(lambda: self.output.insert('6'))
-     self.digit7.clicked.connect(lambda: self.output.insert('7'))
-     self.digit8.clicked.connect(lambda: self.output.insert('8'))
-     self.digit9.clicked.connect(lambda: self.output.insert('9'))
-
-
+    self.digit.clicked.connect(lambda:self.output.insert('+'))
+    self.digit__.clicked.connect(lambda:self.output.insert('-'))
+    self.digit___.clicked.connect(lambda:self.output.insert('*'))
+    self.digit____.clicked.connect(lambda:self.output.insert('/'))
+    self.digit0.clicked.connect(lambda: self.output.insert('0'))
+    self.digit00.clicked.connect(lambda: self.output.insert('00'))
+    self.digit1.clicked.connect(lambda: self.output.insert('1'))
+    self.digit2.clicked.connect(lambda: self.output.insert('2'))
+    self.digit3.clicked.connect(lambda: self.output.insert('3'))
+    self.digit4.clicked.connect(lambda: self.output.insert('4'))
+    self.digit5.clicked.connect(lambda: self.output.insert('5'))
+    self.digit6.clicked.connect(lambda: self.output.insert('6'))
+    self.digit7.clicked.connect(lambda: self.output.insert('7'))
+    self.digit8.clicked.connect(lambda: self.output.insert('8'))
+    self.digit9.clicked.connect(lambda: self.output.insert('9'))
 
 
-     self.setStyleSheet( """
+
+
+    self.setStyleSheet( """
     QPushButton {
         background-color: #f0f0f0;   
         border: 1px solid #b3b3b3;  
@@ -105,13 +105,13 @@ class Calculator(QWidget):
     QPushButton:pressed {
         background-color: #d0d0d0; 
     }
-"""
-)
+    """
+    )
 
-#set text: replaces former digit
-#insert:insert new digit
-     self.backspace.clicked.connect(self.backspace_background_code)
-     self.clear.clicked.connect(self.clear_background_code)
+    #set text: replaces former digit
+    #insert:insert new digit
+    self.backspace.clicked.connect(self.backspace_background_code)
+    self.clear.clicked.connect(self.clear_background_code)
 
     def backspace_background_code(self):
         text=self.output.text()
