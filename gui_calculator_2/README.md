c09d076ae0b390f985900f3e6567e9bc2469f262
Sulaimon <abodunrinsulaimon1@gmail.com>
2025-09-13 15:40:33 +0100

    Calculator can now calculate

---
+++ gui_calculator_2/README.md
@@ -0,0 +1,56 @@
+# GUI Calculator
+
+### Packege used:
+`PyQt5`
+
+### Programming Language
+<p>Python</p>
+
+### How to use
+Clone this Repo
+```bash
+
+$ git clone <repo>
+
+```
+Change dir to the project folder
+```bash
+
+$ cd GUI-projects/gui_calculator_2/
+
+```
+
+Create virtual environment
+```bash
+
+$ python3 -m venv env
+
+```
+
+Activate virtual environment
+```bash
+
+$ source env/bin/activate
+
+or on Windows
+
+\env\Script\activate
+
+```
+Install pakages
+```bash
+
+$ pip install -r requirements.txt
+
+```
+Run program
+```bash
+$ python gui_calculator_prototype.py 
+
+```
+
+## Feature
+[x] Basic Calculation 
+[x] Crash Proof
+[x] Double screens
+[x] Calculation Screen and Error display screen
--- gui_calculator_2/gui_calculator_prototype.py
+++ gui_calculator_2/gui_calculator_prototype.py
@@ -5,11 +5,13 @@ from calculator.addition.add import add
 from calculator.division.divide import divide
 from calculator.multiplication.multiply import multiply
 from calculator.subtraction.subtract import subtract
+from calculator.evaluation.evaluate import sum_multiple_operators, detect_operators
 
 class Calculator(QWidget):
     def __init__(self):
         super().__init__()
         self.output=QLineEdit()
+        self.output2=QLineEdit()
         self.digit=QPushButton('+',self)
         self.digit__= QPushButton('-', self)
         self.digit____= QPushButton('/', self)
@@ -26,101 +28,125 @@ class Calculator(QWidget):
         self.digit8= QPushButton('8', self)
         self.digit9= QPushButton('9', self)
         self.digit_ = QPushButton('=', self)
-        self.backspace= QPushButton('x',self)
+        self.backspace= QPushButton('C', self)
         self.clear = QPushButton('CLR', self)
 
         self.initui()
 
     def initui(self):
-    #Calculator screen
-    vbox=QVBoxLayout()
+        #Calculator screen
+        vbox=QVBoxLayout()
 
-    vbox.addWidget(self.output)
+        vbox.addWidget(self.output)
+        vbox.addWidget(self.output2)
 
-     #row1
-    row1=QHBoxLayout()
-    row1.addWidget(self.digit)
-    row1.addWidget(self.digit__)
-    row1.addWidget(self.digit___)
-    row1.addWidget(self.digit____)
-    row1.addWidget(self.backspace)
-    row1.addWidget(self.clear)
-    vbox.addLayout(row1)
-    #row2
-    row2=QHBoxLayout()
-    row2.addWidget(self.digit0)
-    row2.addWidget(self.digit1)
-    row2.addWidget(self.digit2)
-    row2.addWidget(self.digit3)
-    vbox.addLayout(row2)
-    #row3
-    row3=QHBoxLayout()
-    row3.addWidget(self.digit4)
-    row3.addWidget(self.digit5)
-    row3.addWidget(self.digit6)
-    row3.addWidget(self.digit7)
-    vbox.addLayout(row3)
-    #row4
-    row4=QHBoxLayout()
-    row4.addWidget(self.digit8)
-    row4.addWidget(self.digit9)
-    row4.addWidget(self.digit00)
-    row4.addWidget(self.digit_)
-    vbox.addLayout(row4)
+         #row1
+        row1=QHBoxLayout()
+        row1.addWidget(self.digit)
+        row1.addWidget(self.digit__)
+        row1.addWidget(self.digit___)
+        row1.addWidget(self.digit____)
+        row1.addWidget(self.backspace)
+        row1.addWidget(self.clear)
+        vbox.addLayout(row1)
+        #row2
+        row2=QHBoxLayout()
+        row2.addWidget(self.digit0)
+        row2.addWidget(self.digit1)
+        row2.addWidget(self.digit2)
+        row2.addWidget(self.digit3)
+        vbox.addLayout(row2)
+        #row3
+        row3=QHBoxLayout()
+        row3.addWidget(self.digit4)
+        row3.addWidget(self.digit5)
+        row3.addWidget(self.digit6)
+        row3.addWidget(self.digit7)
+        vbox.addLayout(row3)
+        #row4
+        row4=QHBoxLayout()
+        row4.addWidget(self.digit8)
+        row4.addWidget(self.digit9)
+        row4.addWidget(self.digit00)
+        row4.addWidget(self.digit_)
+        vbox.addLayout(row4)
 
-    self.setLayout(vbox)
-    self.output.setMinimumHeight(60)
+        self.setLayout(vbox)
+        self.output.setMinimumHeight(60)
 
-    self.digit.clicked.connect(lambda:self.output.insert('+'))
-    self.digit__.clicked.connect(lambda:self.output.insert('-'))
-    self.digit___.clicked.connect(lambda:self.output.insert('*'))
-    self.digit____.clicked.connect(lambda:self.output.insert('/'))
-    self.digit0.clicked.connect(lambda: self.output.insert('0'))
-    self.digit00.clicked.connect(lambda: self.output.insert('00'))
-    self.digit1.clicked.connect(lambda: self.output.insert('1'))
-    self.digit2.clicked.connect(lambda: self.output.insert('2'))
-    self.digit3.clicked.connect(lambda: self.output.insert('3'))
-    self.digit4.clicked.connect(lambda: self.output.insert('4'))
-    self.digit5.clicked.connect(lambda: self.output.insert('5'))
-    self.digit6.clicked.connect(lambda: self.output.insert('6'))
-    self.digit7.clicked.connect(lambda: self.output.insert('7'))
-    self.digit8.clicked.connect(lambda: self.output.insert('8'))
-    self.digit9.clicked.connect(lambda: self.output.insert('9'))
+        self.digit.clicked.connect(lambda:self.output.insert(' + '))
+        self.digit__.clicked.connect(lambda:self.output.insert(' - '))
+        self.digit___.clicked.connect(lambda:self.output.insert(' * '))
+        self.digit____.clicked.connect(lambda:self.output.insert(' / '))
+        self.digit0.clicked.connect(lambda: self.output.insert('0'))
+        self.digit00.clicked.connect(lambda: self.output.insert('00'))
+        self.digit1.clicked.connect(lambda: self.output.insert('1'))
+        self.digit2.clicked.connect(lambda: self.output.insert('2'))
+        self.digit3.clicked.connect(lambda: self.output.insert('3'))
+        self.digit4.clicked.connect(lambda: self.output.insert('4'))
+        self.digit5.clicked.connect(lambda: self.output.insert('5'))
+        self.digit6.clicked.connect(lambda: self.output.insert('6'))
+        self.digit7.clicked.connect(lambda: self.output.insert('7'))
+        self.digit8.clicked.connect(lambda: self.output.insert('8'))
+        self.digit9.clicked.connect(lambda: self.output.insert('9'))
 
+        # Display Result
+        # self.digit_.clicked.connect(lambda: self.output2.clear() if self.output2.text() else self.output2.insert(self.perform_calculations(self.output.text())))
 
+        self.digit_.clicked.connect(lambda: self.output.insert(self.perform_calculations(self.output.text()))) # if self.output.text() and self.output.clear() )
 
+        self.setStyleSheet( """
+        QPushButton {
+            background-color: #f0f0f0;   
+            border: 1px solid #b3b3b3;  
+            border-radius: 8px;         
+            padding: 10px;              
+            font-size: 16px;           
+            font-weight: bold;
+        }
+        QPushButton:hover {
+            background-color: #e0e0e0;  
+        }
+        QPushButton:pressed {
+            background-color: #d0d0d0; 
+        }
+        """
+        )
 
-    self.setStyleSheet( """
-    QPushButton {
-        background-color: #f0f0f0;   
-        border: 1px solid #b3b3b3;  
-        border-radius: 8px;         
-        padding: 10px;              
-        font-size: 16px;           
-        font-weight: bold;
-    }
-    QPushButton:hover {
-        background-color: #e0e0e0;  
-    }
-    QPushButton:pressed {
-        background-color: #d0d0d0; 
-    }
-    """
-    )
-
-    #set text: replaces former digit
-    #insert:insert new digit
-    self.backspace.clicked.connect(self.backspace_background_code)
-    self.clear.clicked.connect(self.clear_background_code)
+        #set text: replaces former digit
+        #insert:insert new digit
+        self.backspace.clicked.connect(self.backspace_background_code)
+        self.clear.clicked.connect(self.clear_background_code)
 
     def backspace_background_code(self):
         text=self.output.text()
         self.output.setText(text[:-1])
 
     def clear_background_code(self):
-        text = self.output.text()
         self.output.setText('')
+        self.output2.setText('')
 
+
+    def no_string_input(self):
+        pass
+
+    def get_equations(self, input_data):
+        operation = ["+", "-", "/", "*"]
+        input_list = input_data.split(" ")
+        return input_list
+
+    def perform_calculations(self, user_input):
+        try:
+
+            self.output.clear()
+            self.output2.clear()
+            equation_list = self.get_equations(user_input)
+            useage, operator = detect_operators(equation_list)
+            results = sum_multiple_operators(equation_list)
+            return str(f"{results:,}")
+
+        except Exception as e:
+            self.output2.insert(str(e))
     
 
 
